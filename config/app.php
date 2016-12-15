aba50756e77fd80210a1555f4b71f1c6b045d95d
Matheus Fernandes <matheus@digicriativa.com.br>
2016-12-15 12:17:54 +0000

    feita as configuracoes da lib i5-repository

--- config/app.php
+++ config/app.php
@@ -144,6 +144,8 @@ return [
         CodeDelivery\Providers\AuthServiceProvider::class,
         CodeDelivery\Providers\EventServiceProvider::class,
         CodeDelivery\Providers\RouteServiceProvider::class,
+        
+        Prettus\Repository\Providers\RepositoryServiceProvider::class,
 
     ],
 
