a95b8accb387c9de08051361ca99cc7856196166
Matheus Fernandes <matheus@digicriativa.com.br>
2016-12-20 22:04:16 +0000

    Listagem de categoria implementado

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -7,10 +7,21 @@ use Illuminate\Http\Request;
 use CodeDelivery\Http\Requests;
 use CodeDelivery\Http\Controllers\Controller;
 
+use CodeDelivery\Repositories\CategoryRepository;
+
 class CategoriesController extends Controller
 {
+    private $categoryRepository;
+    
+    public function __construct(CategoryRepository $repository)
+    {
+        $this->categoryRepository = $repository;
+    }
+    
     public function index()
     {
-        return view('admin.categories.index');
+        $categories = $this->categoryRepository->all();
+        
+        return view('admin.categories.index', compact('categories'));
     }
 }
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,1 +1,31 @@
-<h1>Categories</h1>
+@extends('app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h3>Categories</h3>
+                <br>
+                <a href="#" class="btn btn-primary">Nova Categoria</a>
+                <table class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>#</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($categories as $category)
+                            <tr>
+                                <td>{{$category->id}}</td>
+                                <td>{{$category->name}}</td>
+                                <td></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
